44a50e4aae808d9c1b83c2bdeba6ad5ef17e1f51
rikirikandi <rikirikandi1242@gmail.com>
2024-02-25 22:04:00 +0700

    menambahkan tugas 1

--- tugas/tugas1/tugas1a.php
+++ tugas/tugas1/tugas1a.php
@@ -1,2 +1,7 @@
+<?php
 
+$topi = "Topi";
+$bundar = "Bundar";
+
+echo '<h2> <i> <b> "' . $topi . ' saya ' . $bundar . ', ' . $bundar . ' ' .  $topi . ' saya."</h2></i><b>';
 
--- tugas/tugas1/tugas1b.php
+++ tugas/tugas1/tugas1b.php
@@ -1,2 +1,13 @@
-
+<?php
+ $angka = 25;
+    echo "Aku adalah angka <b> $angka";
+    echo "</b><br>";
+    echo "Jika aku dikali 5, maka aku sekarang menjadi <b>" . ( $angka * 5);
+    echo "</b><br>";
+    echo "jika aku dibagi 2, maka aku sekarang akan menjadi <b>" . ($angka * 5 / 2);
+    echo "</b></br>";
+    echo "jika aku ditambah 75, maka aku sekarang menjadi <b>" . ($angka * 5 / 2 + 75);
+    echo "</b><br>";
+    echo "jika aku dikurang 20, maka aku sekarang menjadi <b>" . ($angka * 5 / 2 + 75 - 20);
+    
     
